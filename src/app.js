c8ae89ae205b6a8606e705718af1829dd98d8d30
pupato13 <pupato.diego@gmail.com>
2020-06-17 21:19:03 +1200

    Finishing Challenge. Adding more features than the requirements. Needs to implement a validation for Update and Create methods.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -8,78 +8,210 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+const errors = [];
+
+function isIdValid(request, response, next) {
+	const { id } = request.params;
+
+	if (!isUuid(id)) {
+		//return notifyError(response, "Repo Id is invalid!");
+		return response.status(400).json({ error: "Repo Id is invalid!" });
+	}
+
+	return next();
+}
+
+app.use("/repositories/:id", isIdValid);
 
 app.get("/repositories", (request, response) => {
-    if (repositories.length === 0) {
-        return response.json(
-            "No data available in repositories! You can starting adding a new Repo!"
-        );
-    }
+	if (repositories.length === 0) {
+		// return customResponse(
+		// 	response,
+		// 	"No data available in repositories! You can starting adding a new Repo!",
+		// 	200
+		// );
+		return response.json(
+			"No data available in repositories! You can starting adding a new Repo!"
+		);
+	}
 
-    let { title, tech } = request.query;
+	let { title, tech } = request.query;
 
-    let results = title
-        ? repositories.filter((repo) =>
-              repo.title.toLowerCase().includes(title.toLowerCase())
-          )
-        : repositories;
+	let results = filterRepositoriesByTitle(title);
 
-    // TODO: Filter Techs using array of strings
-    // results = tech
-    //     ? repositories.filter((repo) =>
-    //           repo.title.toLowerCase().includes(tech.toLowerCase())
-    //       )
-    //     : repositories;
+	if (tech) {
+		results = filterRepositoriesByTech(results, tech);
+	}
 
-    // Add Sort by Title or Likes, Order by ASC or DESC
+	// Add Sort by Title or Likes, Order by ASC or DESC
 
-    return response.json(results);
+	// Add Pagination
+
+	//return customResponse(response, results, 200);
+	return response.json(results);
 });
 
+const filterRepositoriesByTitle = (title) => {
+	let results = title
+		? repositories.filter((repo) =>
+				repo.title.toLowerCase().includes(title.toLowerCase())
+		  )
+		: repositories;
+
+	return results;
+};
+
+const filterRepositoriesByTech = (results, tech) => {
+	results = results.filter((repo) => {
+		let techsLowerCase = repo.techs.map((tech) => tech.toLowerCase());
+
+		let found = techsLowerCase.some((techItem) =>
+			techItem.includes(tech.toLowerCase())
+		);
+
+		if (found) {
+			return repo;
+		}
+	});
+
+	return results;
+};
+
 app.post("/repositories", (request, response) => {
-    let { title, url, techs } = request.body;
+	let { title, url, techs } = request.body;
 
-    let repo = {
-        id: uuid(),
-        title,
-        url,
-        techs,
-        likes: 0,
-    };
+	// TODO
+	// Check if title or url exists
 
-    repositories.push(repo);
+	let repo = {
+		id: uuid(),
+		title,
+		url,
+		techs,
+		likes: 0,
+	};
 
-    return response.json(repo);
+	repositories.push(repo);
+
+	//return customResponse(response, repo, 201);
+	return response.json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-    // TODO
+	let { id } = request.params;
+	let { title, url, techs } = request.body;
+
+	let repoIndex = repositories.findIndex((repo) => repo.id === id);
+
+	if (repoIndex < 0) {
+		//return notifyError(response, "Repo not found!");
+		return response.status(400).json({ error: "Repo not found" });
+	}
+
+	let repo = getRepo(repoIndex);
+
+	//updateRepo(repo, title, url, techs);
+	repo = {
+		...repo,
+		title,
+		url,
+		techs,
+	};
+
+	repositories[repoIndex] = repo;
+
+	//return customResponse(response, repo, 200);
+	return response.json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-    // TODO
+	let { id } = request.params;
+
+	let repoIndex = repositories.findIndex((repo) => repo.id === id);
+
+	if (repoIndex < 0) {
+		//return notifyError(response, "Repo not found!");
+
+		return response.status(400).json({ error: "Repo not found" });
+	}
+
+	deleteRepo(repoIndex);
+
+	//return customResponse(response, "Repo deleted!", 200);
+	return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-    let { id } = request.params;
+	let { id } = request.params;
 
-    let repoIndex = repositories.findIndex((repo) => repo.id === id);
+	let repoIndex = repositories.findIndex((repo) => repo.id === id);
 
-    if (repoIndex < 0) {
-        return response.status(400).json({ error: "Repo not found!" });
-    }
+	if (repoIndex < 0) {
+		//return notifyError(response, "Repo not found!");
+		return response.status(400).json({ error: "Repo not found" });
+	}
 
-    let repo = { ...repositories[repoIndex] };
+	let repo = getRepo(repoIndex);
 
-    console.log(repo);
+	addLikeRepo(repo);
 
-    repo.likes++;
+	repositories[repoIndex] = repo;
 
-    repositories[repoIndex] = repo;
-
-    console.log(repo);
-
-    return response.json(repo);
+	//return customResponse(response, repo, 201);
+	return response.json(repo);
 });
+
+const addLikeRepo = (repo) => repo.likes++;
+
+const getRepo = (repoIndex) => ({ ...repositories[repoIndex] });
+
+const updateRepo = (repo, title, url, techs) => {
+	console.log(repo);
+
+	repo = {
+		...repo,
+		title,
+		url,
+		techs,
+	};
+
+	console.log(repo);
+};
+
+const deleteRepo = (repoIndex) => {
+	repositories.splice(repoIndex, 1);
+};
+
+const customResponse = (response, data, statusCode, success = true) => {
+	let customResponse = {
+		success: success,
+		data: data,
+		errors: [...errors],
+	};
+
+	if (success) {
+		delete customResponse.errors;
+	} else {
+		delete customResponse.data;
+	}
+
+	clearErrors();
+
+	return response.status(statusCode).json(customResponse);
+};
+
+const notifyError = (response, errorMessage) => {
+	addError(errorMessage);
+
+	return customResponse(response, null, 400, false);
+};
+
+const addError = (errorMessage) => {
+	if (errorMessage) {
+		errors.push(errorMessage);
+	}
+};
+
+const clearErrors = () => (errors.length = []);
 
 module.exports = app;
